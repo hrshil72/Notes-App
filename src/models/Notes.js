04eb7698157e4c85bfa299d527b31ac40b836608
[Harshil Karir] <harshilkarir72@gmail.com>
2024-03-27 19:26:35 +0530

    displaying notes and pagination done

---
+++ src/models/Notes.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const notesSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Notes", notesSchema);
--- src/models/User.js
+++ src/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now,
+    default: Date.now(),
   },
 });
 
